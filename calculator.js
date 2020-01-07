5ba770d50bd3cd35f074f4071c1574525d2c9dd8
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-01-07 13:20:12 +0100

    add path.js

--- calculator.js
+++ calculator.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello Express, be nice to me")
-})
+    res.sendFile(__dirname + "/index.html");
+});
 app.listen(3000, () => {
     console.log("Server start on port 3000");
 });
